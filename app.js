681849f414433928e62e0aaa6bce5ddc73ce40ef
hardzc123 <zhangchihust@gmail.com>
2021-03-03 14:55:14 +0100

    stable version

--- app.js
+++ app.js
@@ -36,8 +36,8 @@ app.post("/", function(req, res){
   items.push(item);
   console.log(item);
   res.redirect("/");
-})
+});
 
-app.listen(3000, function() {
-  console.log("Server started on port 3000.");
+app.listen(3100, function() {
+  console.log("Server started on port 3100.");
 });
